b99f73f9df30d1d405d7a8c3cf9203ae19c946dd
Hans Elizaga <tom.elizaga@gmail.com>
2023-05-05 21:31:08 -0700

    added boilerpipe as web scraper (need java)

--- utils.js
+++ utils.js
@@ -1,40 +1,21 @@
-import axios from "axios";
-import { load } from "cheerio";
-
 // This function scrapes the web content from a given URL and cleans it.
+// utils.js
+import boiler from "boilerpipe-scraper";
+
 export async function scrapeWebContent(url) {
   try {
-    const { data } = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
-      },
-    });
-    const $ = load(data);
-
-    // Remove unnecessary elements using an array of selectors
-    const selectorsToRemove = [
-      "script",
-      "style",
-      "header",
-      "footer",
-      "nav",
-      ".nav",
-      ".footer",
-      ".header",
-      ".advertisement",
-      ".sidebar",
-      ".comments",
-      // Add more selectors as needed
-    ];
-
-    selectorsToRemove.forEach((selector) => {
-      $(selector).remove();
+    const text = await new Promise((resolve, reject) => {
+      boiler(url, (err, text) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(text);
+        }
+      });
     });
 
     // Remove extra spaces, newline characters, and other unwanted patterns
-    const cleanedText = $("body")
-      .text()
+    const cleanedText = text
       .trim()
       .replace(/\s\s+/g, " ")
       .replace(/\n/g, " ")
