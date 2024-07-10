c302d3805badcc380f75299b53774677b2b60dc0
Hans Elizaga <tom.elizaga@gmail.com>
2024-07-10 15:35:12 -0700

    changed to vite and updated dependencies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,8 @@
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import authConfig from "./config/auth0-config.json";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { createRoot } from "react-dom/client";
-import React from "react";
 
 const theme = createTheme({
   palette: {
